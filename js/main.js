adf30f9def65962160c6298ff99a8b7b28fb0729
DoanhHoang <hoangngocdoanh031200@gmail.com>
2022-11-10 22:33:10 +0700

    first commit

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ function firstQuestion() {
     Swal.fire({
         title: CONFIG.introTitle,
         text: CONFIG.introDesc,
-        imageUrl: 'img/logi.gif',
+        imageUrl: 'img/hung.jpg',
         imageWidth: 300,
         imageHeight: 300,
         background: '#fff url("img/iput-bg.jpg")',
